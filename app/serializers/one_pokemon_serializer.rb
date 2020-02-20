82dcb9ee527ee5e1f447fb1b50275f2270b285c6
Levi Miller <paullevimiller@gmail.com>
2020-02-19 16:42:06 -0800

    Add flavor text to serializer

--- app/serializers/one_pokemon_serializer.rb
+++ app/serializers/one_pokemon_serializer.rb
@@ -6,5 +6,5 @@ class OnePokemonSerializer
 
   attributes :abilities, :base_experience, :forms, :game_indices, :height, 
     :held_items, :pokemon_id, :is_default, :location_area_encounters, :moves, 
-    :name, :order, :species, :sprites, :stats, :types, :weight
+    :name, :order, :species, :sprites, :stats, :types, :weight, :flavor_text
 end
